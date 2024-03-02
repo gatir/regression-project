48b6a0cef4122a82d6423bc051d4357826c9980f
gatir <mm.pourdad@gmail.com>
2024-03-02 16:49:01 +0330

    empty cell cleared and saved

--- project2.py
+++ project2.py
@@ -1,3 +1,7 @@
 import pandas as pd
-df = pd.read_csv('tehranhdata.csv')
+df = pd.read_csv(
+    "E:/learning/py-ml-jadi/jadimlgit/regression-project/tehranhdata.csv")
 new_df = df.dropna()
+new_df.to_csv(
+    "E:/learning/py-ml-jadi/jadimlgit/regression-project/tehranhdata2.csv")
+# empty cell cleared and saved
